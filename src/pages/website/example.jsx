82855f81b92a2d0ae718b975e57a0335c6319590
ahmed iblao <ahmediblao53@gmail.com>
2023-11-11 23:30:38 +0300

    new updates

--- src/pages/website/example.jsx
+++ src/pages/website/example.jsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom";
 
 
 export default function Example() {
-   
+   fetch("http://127.0.0.1:8000/api/user/show")
+   .then(res => res.json() ) .then(data => data.map(user => console.log(user.email)))
+   .catch(err => console.log(err))
     return(
         <div style={{display:"flex" ,justifyContent:"center" , alignItems:"center", fontWeight:"bold"}}>
             

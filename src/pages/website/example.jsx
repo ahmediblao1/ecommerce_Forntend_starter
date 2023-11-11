688634597716455309751eb41108273d9c347ace
ahmed iblao <ahmediblao53@gmail.com>
2023-11-11 23:34:27 +0300

    new updates

--- src/pages/website/example.jsx
+++ src/pages/website/example.jsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
 
-export default function Example() {
-   fetch("http://127.0.0.1:8000/api/user/show")
-   .then(res => res.json() ) .then(data => data.map(user => console.log(user.email)))
-   .catch(err => console.log(err))
+ export default  function Example() {
+//    try {
+//      let users = await fetch("http://127.0.0.1:8000/api/user/show")
+//      console.log( await users.json())
+
+//  } catch (error) {
+//     console.log(" there was unexpexted arror "`${error}`) 
+//  }
+    
     return(
         <div style={{display:"flex" ,justifyContent:"center" , alignItems:"center", fontWeight:"bold"}}>
             

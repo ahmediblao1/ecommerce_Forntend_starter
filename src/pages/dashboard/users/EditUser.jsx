5a8c0fd9110c83554cf4d3a011c5d0d3f0d741c3
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-01-24 23:20:35 +0300

    Update EditUser.jsx

--- src/pages/dashboard/users/EditUser.jsx
+++ src/pages/dashboard/users/EditUser.jsx
@@ -9,7 +9,6 @@ import Form from "../../../components/Form";
 export default function EditUser() {
     const [name,setname] = useState('')
     const [email,setemail] = useState('')
-   
 const id = window.location.pathname.split("/").slice(-1)[0]
 useEffect(() => {
     fetch(`http://127.0.0.1:8000/api/user/showbyid/${id}`)

bd2b90de34d01d3418d82441fe8dfe906e3158f5
ahmed iblao <98063069+ahmediblao1@users.noreply.github.com>
2024-03-29 23:55:52 +0300

    Update Home.jsx

--- src/pages/website/Home.jsx
+++ src/pages/website/Home.jsx
@@ -3,7 +3,7 @@ import Hedear from "../../components/Hedear";
 export default function Home(){
     return(
         <div>
-            
+            <Hedear />
             <h1 className="home">HOME</h1>
         </div>
     )
